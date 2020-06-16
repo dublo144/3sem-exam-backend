ca53c6a0103f63da0e89d2dc1bb424ca74f7b353
Mads Brandt <mads.brandt93@gmail.com>
2020-06-17 01:42:50 +0200

    Commented out ApplicationConfig

--- src/main/java/rest/ApplicationConfig.java
+++ src/main/java/rest/ApplicationConfig.java
@@ -16,32 +16,32 @@ import java.util.Set;
 @javax.ws.rs.ApplicationPath("api")
 public class ApplicationConfig extends Application {
 
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new java.util.HashSet<>();
-        resources.add(OpenApiResource.class);
-        addRestResourceClasses(resources);
-        return resources;
-    }
-
-    /**
-     * Do not modify addRestResourceClasses() method.
-     * It is automatically populated with
-     * all resources defined in the project.
-     * If required, comment out calling this method in getClasses().
-     */
-    private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
-        resources.add(CorsRequestFilter.class);
-        resources.add(CorsResponseFilter.class);
-        resources.add(errorhandling.GenericExceptionMapper.class);
-        resources.add(security.JWTAuthenticationFilter.class);
-        resources.add(LoginEndpoint.class);
-        resources.add(RecipeResource.class);
-        resources.add(MenuPlanResource.class);
-        resources.add(security.RolesAllowedFilter.class);
-        resources.add(AuthenticationExceptionMapper.class);
-        resources.add(UserExceptionMapper.class);
-    }
+//    @Override
+//    public Set<Class<?>> getClasses() {
+//        Set<Class<?>> resources = new java.util.HashSet<>();
+//        resources.add(OpenApiResource.class);
+//        addRestResourceClasses(resources);
+//        return resources;
+//    }
+//
+//    /**
+//     * Do not modify addRestResourceClasses() method.
+//     * It is automatically populated with
+//     * all resources defined in the project.
+//     * If required, comment out calling this method in getClasses().
+//     */
+//    private void addRestResourceClasses(Set<Class<?>> resources) {
+//        resources.add(org.glassfish.jersey.server.wadl.internal.WadlResource.class);
+//        resources.add(CorsRequestFilter.class);
+//        resources.add(CorsResponseFilter.class);
+//        resources.add(errorhandling.GenericExceptionMapper.class);
+//        resources.add(security.JWTAuthenticationFilter.class);
+//        resources.add(LoginEndpoint.class);
+//        resources.add(RecipeResource.class);
+//        resources.add(MenuPlanResource.class);
+//        resources.add(security.RolesAllowedFilter.class);
+//        resources.add(AuthenticationExceptionMapper.class);
+//        resources.add(UserExceptionMapper.class);
+//    }
 
 }
